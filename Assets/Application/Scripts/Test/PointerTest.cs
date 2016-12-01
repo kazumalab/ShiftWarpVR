b25d622eecb08191310c9031e6a3baa5791569b0
Kazuma Takeda <kazuma-arashi@hotmail.co.jp>
2016-12-01 17:10:22 +0900

    Add Test Code.

---
+++ Assets/Application/Scripts/Test/PointerTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerTest : MonoBehaviour {
+
+	private LineRenderer pointer;
+
+	private void Start () {
+		pointer = this.GetComponent<LineRenderer>();
+	}
+}
